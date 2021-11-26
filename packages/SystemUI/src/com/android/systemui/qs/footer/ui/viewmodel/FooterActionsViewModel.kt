4151027015662e21eae7d7f45c94a8bf6cb60b48
Ido Ben-Hur <idoybh2@gmail.com>
2021-11-26 18:42:20 +0200

    QSFooter: Launch Personalizations when long clicking settings
    Squashed:
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Tue, 9 Apr 2024 12:27:46 +0300
        Subject: [PATCH 285/820] SystemUI: Adapt long press on QS settings to launch Personalizations
    
        Add support for adding an onLongPress Unit to the Expandable for view
        AOSP now uses it by default for the settings button
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    neobuddy89: Updated for A15 QPR2, A16
    
    Change-Id: Ibbec3b5386fafd2adf06ba696e2adb421bfc49de
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
@@ -244,6 +244,15 @@ fun createFooterActionsViewModel(
         footerActionsInteractor.showSettings(expandable)
     }
 
+    fun onSettingsButtonLongClicked(expandable: Expandable) {
+        if (falsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
+            return
+        }
+
+        footerActionsInteractor.showCustomSettings(expandable)
+        return
+    }
+
     fun onPowerButtonClicked(expandable: Expandable) {
         if (keyguardStateController.isShowing() && keyguardStateController.isMethodSecure() 
                 && Settings.System.getIntForUser(appContext.getContentResolver(),
@@ -289,7 +298,7 @@ fun createFooterActionsViewModel(
     val userSwitcher =
         userSwitcherViewModel(qsThemedContext, footerActionsInteractor, ::onUserSwitcherClicked)
 
-    val settings = settingsButtonViewModel(qsThemedContext, ::onSettingsButtonClicked)
+    val settings = settingsButtonViewModel(qsThemedContext, ::onSettingsButtonClicked, ::onSettingsButtonLongClicked)
 
     val power =
         if (showPowerButton) {
@@ -412,6 +421,7 @@ private fun userSwitcherContentDescription(
 fun settingsButtonViewModel(
     qsThemedContext: Context,
     onSettingsButtonClicked: (Expandable) -> Unit,
+    onSettingsButtonLongClicked: (Expandable) -> Unit,
 ): FooterActionsButtonViewModel {
     return FooterActionsButtonViewModel(
         id = R.id.settings_button_container,
@@ -422,6 +432,7 @@ fun settingsButtonViewModel(
         iconTint = Utils.getColorAttrDefaultColor(qsThemedContext, R.attr.onShadeInactiveVariant),
         backgroundColor = R.attr.shadeInactive,
         onSettingsButtonClicked,
+        onSettingsButtonLongClicked,
     )
 }
 
