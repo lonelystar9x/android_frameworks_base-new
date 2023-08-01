95b2a1dccc64363b417fd21fffa1d7ac063fb55e
Ido Ben-Hur <idoybh2@gmail.com>
2023-08-01 15:20:29 +0300

    base: Allow configuring haptics for wake gestures [1/2]
    Change-Id: I834ae394e3fc0077622e4e6f5896163d165d8e22

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -134,6 +134,12 @@ public class AmbientDisplayConfiguration {
     }
 
     /** @hide */
+    public boolean pickupGestureVibrate(int user) {
+        return boolSettingDefaultOff(Settings.Secure.DOZE_PICK_UP_GESTURE_VIBRATE, user)
+                && pickupGestureEnabled(user);
+    }
+
+    /** @hide */
     public boolean dozePickupSensorAvailable() {
         return mContext.getResources().getBoolean(R.bool.config_dozePulsePickup);
     }
@@ -142,6 +148,12 @@ public class AmbientDisplayConfiguration {
     public boolean tapGestureEnabled(int user) {
         return boolSettingDefaultOn(Settings.Secure.DOZE_TAP_SCREEN_GESTURE, user)
                 && tapSensorAvailable();
+    }
+
+    /** @hide */
+    public boolean tapGestureVibrate(int user) {
+        return boolSettingDefaultOff(Settings.Secure.DOZE_TAP_GESTURE_VIBRATE, user)
+                && tapGestureEnabled(user);
     }
 
     /** @hide */
@@ -169,6 +181,12 @@ public class AmbientDisplayConfiguration {
     public boolean tapGestureAmbient(int user) {
         return boolSettingDefaultOff(Settings.Secure.DOZE_TAP_GESTURE_AMBIENT, user)
                 && tapGestureEnabled(user) && pulseOnNotificationEnabled(user);
+    }
+
+    /** @hide */
+    public boolean doubleTapGestureVibrate(int user) {
+        return boolSettingDefaultOff(Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_VIBRATE, user)
+                && doubleTapGestureEnabled(user);
     }
 
     /** @hide */
