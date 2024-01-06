92dfe11d2080db83ad19cab3fa513c2d4a03ac54
cjh1249131356 <cjh1249131356@gmail.com>
2024-01-06 21:48:51 +0800

    SystemUI: Implement RefreshRateManager [1/2]
    Includes:
    - Per-app refresh rate config
    - Extreme refresh rate (Force all apps to run in maximum refresh rate)
    - Temp refresh rate allowed (Used in MEMC)
    - Refresh rate QS tile
    
    [Arman-ATI: merge with current utils]
    
    Change-Id: I1a5d4ccaf18389a0ea26342bd682856faa3ab17a
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [nurkeinneid: move to fwb and drop sdk extensions/dependencies]
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -290,6 +290,9 @@ import com.android.internal.os.IDropBoxManagerService;
 import com.android.internal.policy.PhoneLayoutInflater;
 import com.android.internal.util.Preconditions;
 
+import org.rising.display.IRefreshRateManagerService;
+import org.rising.display.RefreshRateManager;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -1848,6 +1851,15 @@ public final class SystemServiceRegistry {
                         return new IntrusionDetectionManager(service);
                     }
                 });
+
+        registerService(Context.REFRESH_RATE_MANAGER_SERVICE, RefreshRateManager.class,
+                new CachedServiceFetcher<RefreshRateManager>() {
+            @Override
+            public RefreshRateManager createService(ContextImpl ctx) {
+                IBinder binder = ServiceManager.getService(Context.REFRESH_RATE_MANAGER_SERVICE);
+                IRefreshRateManagerService service = IRefreshRateManagerService.Stub.asInterface(binder);
+                return new RefreshRateManager(ctx.getOuterContext(), service);
+            }});
 
         sInitializing = true;
         try {
--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7552,6 +7552,18 @@ public final class Settings {
         public static final String QS_BRIGHTNESS_SLIDER_SHAPE = "qs_brightness_slider_shape";
 
         /**
+         * Per-app refresh rate config
+         * @hide
+         */
+        public static final String REFRESH_RATE_CONFIG_CUSTOM = "refresh_rate_config_custom";
+
+        /**
+         * Force highest refresh rate in all apps
+         * @hide
+         */
+        public static final String EXTREME_REFRESH_RATE = "extreme_refresh_rate";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -20046,6 +20058,12 @@ public final class Settings {
          * @hide
          */
         public static final String ONE_HANDED_KEYGUARD_SIDE = "one_handed_keyguard_side";
+
+        /**
+         * Whether refresh rate should be switched to 60Hz on power save mode.
+         * @hide
+         */
+        public static final String LOW_POWER_REFRESH_RATE = "low_power_rr_switch";
 
         /**
          * A list of uids that are allowed to use restricted networks.
