ca7ae2dbe6f939390813bae21689d54e077cc69a
Pranav Vashi <neobuddy89@gmail.com>
2025-04-04 22:21:04 +0530

    SystemUI: Remove more unnecessary smartspace spaces
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/ClockSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/ClockSection.kt
@@ -203,15 +203,7 @@ constructor(
                 TOP,
             )
             val largeClockTopMargin =
-                if (com.android.systemui.shared.Flags.clockReactiveSmartspaceLayout()) {
-                    keyguardClockViewModel.getLargeClockTopMargin() +
-                        getDimen(ENHANCED_SMARTSPACE_HEIGHT)
-                } else if (smartspaceViewModel.isSmartspaceEnabled) {
-                    keyguardClockViewModel.getLargeClockTopMargin() +
-                        getDimen(DATE_WEATHER_VIEW_HEIGHT)
-                } else {
-                    keyguardClockViewModel.getLargeClockTopMargin()
-                }
+                keyguardClockViewModel.getLargeClockTopMargin()
             connect(
                 customR.id.lockscreen_clock_view_large,
                 TOP,
@@ -262,12 +254,9 @@ constructor(
                     }
 
             if (dateWeatherBelowSmallClock) {
-                val dateWeatherSmartspaceHeight =
-                    getDimen(context, DATE_WEATHER_VIEW_HEIGHT).toFloat()
                 clockInteractor.setNotificationStackDefaultTop(
-                    smallClockBottom +
-                        dateWeatherSmartspaceHeight +
-                        marginBetweenSmartspaceAndNotification
+                    (smallClockBottom +
+                        marginBetweenSmartspaceAndNotification).toFloat()
                 )
             } else {
                 clockInteractor.setNotificationStackDefaultTop(
@@ -277,20 +266,5 @@ constructor(
         }
 
         constrainWeatherClockDateIconsBarrier(constraints)
-    }
-
-    private fun getDimen(name: String): Int {
-        return getDimen(context, name)
-    }
-
-    companion object {
-        private const val DATE_WEATHER_VIEW_HEIGHT = "date_weather_view_height"
-        private const val ENHANCED_SMARTSPACE_HEIGHT = "enhanced_smartspace_height"
-
-        fun getDimen(context: Context, name: String): Int {
-            val res = context.packageManager.getResourcesForApplication(context.packageName)
-            val id = res.getIdentifier(name, "dimen", context.packageName)
-            return if (id == 0) 0 else res.getDimensionPixelSize(id)
-        }
     }
 }
