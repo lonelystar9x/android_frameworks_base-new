800d4321205e3a7a8c5e0e54e56ceeacac0ddc43
Pranav Vashi <neobuddy89@gmail.com>
2025-07-19 22:42:13 +0530

    MediaCarouselController: Fix crash on monet theme change
    Log:
        07-19 22:15:13.029 D/AndroidRuntime(8023): Shutting down VM
        07-19 22:15:13.030 E/AndroidRuntime(8023): FATAL EXCEPTION: main
        07-19 22:15:13.030 E/AndroidRuntime(8023): Process: com.android.systemui, PID: 8023
        07-19 22:15:13.030 E/AndroidRuntime(8023): android.content.res.Resources$NotFoundException: Resource ID #0x0
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.content.res.ResourcesImpl.getValue(ResourcesImpl.java:286)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.content.res.Resources.getDimensionPixelSize(Resources.java:848)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at com.android.systemui.media.controls.ui.controller.MediaCarouselController.inflateSettingsButton(go/retraceme aa863320c2b13846d33eab0130c773beb1327765416df347c69788878cad699f:68)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at com.android.systemui.media.controls.ui.controller.MediaCarouselController$configListener$1.onThemeChanged(go/retraceme aa863320c2b13846d33eab0130c773beb1327765416df347c69788878cad699f:7)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at com.android.systemui.statusbar.phone.ConfigurationControllerImpl.onConfigurationChanged(go/retraceme aa863320c2b13846d33eab0130c773beb1327765416df347c69788878cad699f:353)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at com.android.systemui.SystemUIApplication.onConfigurationChanged(go/retraceme aa863320c2b13846d33eab0130c773beb1327765416df347c69788878cad699f:38)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.ConfigurationController.performConfigurationChanged(ConfigurationController.java:263)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.ConfigurationController.handleConfigurationChangedInner(ConfigurationController.java:237)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.ConfigurationController.handleConfigurationChanged(ConfigurationController.java:155)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.ConfigurationController.handleConfigurationChanged(ConfigurationController.java:130)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.ActivityThread.handleConfigurationChanged(ActivityThread.java:6895)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.servertransaction.ConfigurationChangeItem.execute(ConfigurationChangeItem.java:56)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.servertransaction.TransactionExecutor.executeNonLifecycleItem(TransactionExecutor.java:133)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.servertransaction.TransactionExecutor.executeTransactionItems(TransactionExecutor.java:103)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.servertransaction.TransactionExecutor.execute(TransactionExecutor.java:80)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2823)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.os.Handler.dispatchMessage(Handler.java:110)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.os.Looper.loopOnce(Looper.java:248)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.os.Looper.loop(Looper.java:338)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at android.app.ActivityThread.main(ActivityThread.java:9068)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at java.lang.reflect.Method.invoke(Native Method)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:596)
        07-19 22:15:13.030 E/AndroidRuntime(8023): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:932)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaCarouselController.kt
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaCarouselController.kt
@@ -265,13 +265,17 @@ constructor(
             override fun onDensityOrFontScaleChanged() {
                 // System font changes should only happen when UMO is offscreen or a flicker may
                 // occur
-                updatePlayers(recreateMedia = true)
-                inflateSettingsButton()
+                mediaFrame.post {
+                    updatePlayers(recreateMedia = true)
+                    inflateSettingsButton()
+                }
             }
 
             override fun onThemeChanged() {
-                updatePlayers(recreateMedia = true)
-                inflateSettingsButton()
+                mediaFrame.post {
+                    updatePlayers(recreateMedia = true)
+                    inflateSettingsButton()
+                }
             }
 
             override fun onConfigChanged(newConfig: Configuration?) {
@@ -280,16 +284,20 @@ constructor(
             }
 
             override fun onUiModeChanged() {
-                updatePlayers(recreateMedia = false)
-                inflateSettingsButton()
+                mediaFrame.post {
+                    updatePlayers(recreateMedia = false)
+                    inflateSettingsButton()
+                }
             }
 
             override fun onLocaleListChanged() {
                 // Update players only if system primary language changes.
                 if (carouselLocale != context.resources.configuration.locales.get(0)) {
                     carouselLocale = context.resources.configuration.locales.get(0)
-                    updatePlayers(recreateMedia = true)
-                    inflateSettingsButton()
+                    mediaFrame.post {
+                        updatePlayers(recreateMedia = true)
+                        inflateSettingsButton()
+                    }
                 }
             }
         }
