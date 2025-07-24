e732ad1bb39a9b46f527b2235e9caf2cec7d3b1d
Pavel Salomatov <pasalomatov@sberdevices.ru>
2025-07-24 11:43:45 +0300

    bootanimation: init progress font only if we need it
    This prevents artifacts with 1st frame of animation
    
    Change-Id: I28fbe435187000e885c67e8a471a4e62945983d7
    Signed-off-by: Pavel Salomatov <pasalomatov@sberdevices.ru>

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -1482,7 +1482,12 @@ bool BootAnimation::movie() {
         mClockEnabled = clockFontInitialized;
     }
 
-    initFont(&mAnimation->progressFont, PROGRESS_FONT_ASSET);
+    bool progressFontInitialized = false;
+    if (mAnimation->progressEnabled) {
+        progressFontInitialized =
+            initFont(&mAnimation->progressFont, PROGRESS_FONT_ASSET) == NO_ERROR;
+        mAnimation->progressEnabled = progressFontInitialized;
+    }
 
     if (mClockEnabled && !updateIsTimeAccurate()) {
         mTimeCheckThread = new TimeCheckThread(this);
