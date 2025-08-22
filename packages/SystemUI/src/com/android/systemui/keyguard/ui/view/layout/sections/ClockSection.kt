1e9901c25b9c0c597dcf3f7e358fbdae2c16f75b
Pranav Vashi <neobuddy89@gmail.com>
2025-08-23 03:12:06 +0530

    SystemUI: Add margin above small clock in lockscreen
    * Space is too small. It often overlaps with face unlock image.
    * Tweak space above clock and above notifications.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/ClockSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/ClockSection.kt
@@ -231,6 +231,7 @@ constructor(
                     ),
             )
             val smallClockTopMargin = keyguardClockViewModel.getSmallClockTopMargin()
+                    + context.resources.getDimensionPixelSize(R.dimen.keyguard_clock_top_margin)
             create(R.id.small_clock_guideline_top, ConstraintSet.HORIZONTAL_GUIDELINE)
             setGuidelineBegin(R.id.small_clock_guideline_top, smallClockTopMargin)
             connect(customR.id.lockscreen_clock_view, TOP, R.id.small_clock_guideline_top, BOTTOM)
@@ -241,6 +242,7 @@ constructor(
             val smallClockBottom =
                 smallClockTopMargin +
                     context.resources.getDimensionPixelSize(customR.dimen.small_clock_height)
+                    - context.resources.getDimensionPixelSize(R.dimen.keyguard_clock_top_margin)
             val marginBetweenSmartspaceAndNotification =
                 context.resources.getDimensionPixelSize(
                     R.dimen.keyguard_status_view_bottom_margin
