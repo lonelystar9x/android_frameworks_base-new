edd626a432769e2fcb8907a6c1ae45461a22d8b4
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-30 17:37:21 +0800

    SystemUI: QS expansion animation enhance
    fix qs panel showing before complete expansion
    
    Change-Id: I5ce5f5b5424a23b42c1703e2eefccef25fa1cb25
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
+++ packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
@@ -363,7 +363,7 @@ constructor(
                                         }
                                     }
                                 }
-                                .graphicsLayer { alpha = viewModel.viewAlpha }
+                                .graphicsLayer { alpha = viewModel.panelAlpha }
                                 .thenIf(!Flags.notificationShadeBlur()) {
                                     Modifier.offset {
                                         IntOffset(
