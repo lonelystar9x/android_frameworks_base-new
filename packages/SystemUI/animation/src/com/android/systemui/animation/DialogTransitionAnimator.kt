7e38658174ded6d919441cdd1e85bbd04a1c84b0
Dmitrii <bankersenator@gmail.com>
2025-10-03 04:26:06 +0000

    SystemUI: Respect global setting for SysUI dialog blur
    Change-Id: I0550ee273fc2047193bf5c78cd9ac17f02992a5d
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/animation/src/com/android/systemui/animation/DialogTransitionAnimator.kt
+++ packages/SystemUI/animation/src/com/android/systemui/animation/DialogTransitionAnimator.kt
@@ -579,7 +579,7 @@ private class AnimatedDialog(
     private var decorViewLayoutListener: View.OnLayoutChangeListener? = null
 
     private var hasInstrumentedJank = false
-    private val blurUtils = BlurUtils(dialog.context.resources)
+    private val blurUtils = BlurUtils(dialog.context.resources, dialog.context.contentResolver)
 
     fun start() {
         val cuj = controller.cuj
